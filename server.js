cba9f5e7cca99653b97de7c00a1d9188f9615f33
Brandon van Niekerk <vnkbrand@gmail.com>
2018-08-01 13:54:29 +0200

    Final push2

--- server.js
+++ server.js
@@ -1,44 +1,42 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const path = require('path');
 
-const users = require("./routes/api/users");
-const posts = require("./routes/api/posts");
-const profile = require("./routes/api/profile");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
-//Body Parser Middleware
-app.use(bodyParser.urlencoded({extended: false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//  Passport Middleware
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Passport middleware
 app.use(passport.initialize());
 
-//  Passport Config
-require("./config/passport")(passport);
+// Passport Config
+require('./config/passport')(passport);
 
-//DB Congig
-const db = require("./config/keys").mongoURI;
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-//Connect to MongoDB through Mongoose
-mongoose
-.connect(db)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-//Use Routes
-app.use("/api/users", users);
-app.use("/api/posts", posts);
-app.use("/api/profile", profile);
-
-//  This is for deployment to heroku - we will check for production, set the static folder to client build and for any route that gets hit here - we will load react index.html file
-
-//  Server static assets if in production
-if(process.env.NODE_ENV == 'production') {
-  //  Set a static folder
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
@@ -46,7 +44,6 @@ if(process.env.NODE_ENV == 'production') {
   });
 }
 
-
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server Running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
