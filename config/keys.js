e5fb59f8784c6335cd12b94e6bb067302e39a479
Brandon van Niekerk <vnkbrand@gmail.com>
2018-08-01 14:00:43 +0200

    Final push3

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,5 @@
-//  Test to see what environment we are in
-//  When we DEPLOY TO HEROKU, it will be production
-//  When we are in DEV, it will be keys_dev
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
   module.exports = require('./keys_dev');
 }
-
-
