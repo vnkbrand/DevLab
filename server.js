8ceea8052a0497bfa549288280a02c05434cc60b
Brandon van Niekerk <vnkbrand@gmail.com>
2018-08-01 14:34:20 +0200

    Deploy Ready4

--- server.js
+++ server.js
@@ -34,15 +34,14 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+// Server static assets if in Production
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+ }
 
 const port = process.env.PORT || 5000;
 
