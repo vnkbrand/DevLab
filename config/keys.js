794c982ce8df5c54e9c4489d300936337e47404c
Brandon van Niekerk <vnkbrand@gmail.com>
2018-08-01 16:33:14 +0200

    Deploy Ready8

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+module.exports = {
+  mongoURI: 'mongodb://vnkbrand:vnkbra001@ds131711.mlab.com:31711/dev__connector',
+  secretOrKey: "secret"
+};
--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://vnkbrand:vnkbra001@ds131711.mlab.com:31711/dev__connector',
-  secretOrKey: "secret"
-};
