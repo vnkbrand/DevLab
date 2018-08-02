ea5f1aa1f20c8796bd2c1b489aac908e333f1abb
Brandon van Niekerk <vnkbrand@gmail.com>
2018-08-02 13:45:50 +0200

    Final Deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://vnkbrand:vnkbra001@ds131711.mlab.com:31711/dev__connector',
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
